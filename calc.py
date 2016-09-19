0a2da744a1971a3086d03a15b06261348944e4fe
Angel Perea Arias <aperea@gamma03.aulas.gsyc.urjc.es>
2016-09-19 10:47:26 +0200

    Fixed a comment

--- calc.py
+++ calc.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
     elif sys.argv[2] == "multiplica":
         result = multiply(operando1, operando2)
     else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
+        sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
 
     print(result)
