897c009a1dc2539390a21761f06a5063033633ef
Angel Perea Arias <aperea@alpha11.aulas.gsyc.urjc.es>
2016-09-20 12:33:56 +0200

    Created Calcoohija.py and added multiply and divide functions

---
+++ calcoohija.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoo
+
+
+class CalculadoraHija(calcoo.Calculadora):
+    def multiply(self, op1, op2):
+        """ Function to multiply the operands """
+        return op1 * op2
+        
+    def divide(self, op1, op2):
+        """ Function to divide the operands """
+        try:
+            return op1 / op2
+        except:
+            sys.exit("Error: Invalid parameters")
+
+if __name__ == "__main__":
+    try:
+        op1 = int(sys.argv[1])
+        op2 = int(sys.argv[3])
+        
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+        
+    if sys.argv[2] == "suma":
+        result = CalculadoraHija().plus(op1, op2)
+        
+    elif sys.argv[2] == "resta":
+        result = CalculadoraHija().minus(op1, op2)
+        
+    elif sys.argv[2] == "multiplica":
+        result = CalculadoraHija().multiply(op1, op2)
+        
+    elif sys.argv[2] == "divide":
+        result = CalculadoraHija().divide(op1, op2)
+        
+    else:
+        sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
+
+    print(result)
