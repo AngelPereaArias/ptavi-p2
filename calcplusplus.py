6c5752baa9268311961722a3204bf8001e98ec24
Angel Perea Arias <aperea@alpha35.aulas.gsyc.urjc.es>
2016-09-22 11:50:14 +0200

    Created main procedure using csv module

--- calcplusplus.py
+++ calcplusplus.py
@@ -1,2 +1,48 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*
+
+
+import calcoohija
+import sys
+import csv
+
+if __name__ == "__main__":
+
+    lines = csv.reader(open('fichero', newline=''), delimiter=',')
+    for line in lines:
+        #Sprint(' '.join(line))
+        elems = ' '.join(line).split(" ")
+        print(elems)
+        
+        #for elem in range(len(elems)-1):
+        #    print(str(elems[elem+1]))
+
+        result = int(elems[1])
+
+        if elems[0] == "suma":
+            for elem in range(len(elems)-2):
+                print(str(result) + " + " + str(elems[elem+2]))
+                result = calcoohija.CalculadoraHija().plus(int(elems[elem+2]), result)
+            
+            
+        elif elems[0] == "resta":
+            for elem in range(len(elems)-2):
+                print(str(result) + " - " + str(elems[elem+2]))
+                result = calcoohija.CalculadoraHija().minus(result, int(elems[elem+2]))
+            
+
+        elif elems[0] == "multiplica":
+            for elem in range(len(elems)-2):
+                print(str(result) + " * " + str(elems[elem+2]))
+                result = calcoohija.CalculadoraHija().multiply(int(elems[elem+2]), result)
+            
+        elif elems[0] == "divide":
+            for elem in range(len(elems)-2):
+                print(str(result) + " / " + str(elems[elem+2]))
+                result = calcoohija.CalculadoraHija().divide(result, int(elems[elem+2]))
+            
+        else:
+            sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
+
+        print("----------->" + str(result))
+            
