f9e34819ece05e762a9c66809cba23d4bd28046e
Angel Perea Arias <aperea@alpha35.aulas.gsyc.urjc.es>
2016-09-22 12:30:35 +0200

    fixed missing whitespaces

--- calcplus.py
+++ calcplus.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
         document = open(sys.argv[1])
     except ValueError:
         sys.exit("Error: Invalid Document")
-    
+
     lines = document.readlines()
-    
+
     for line in lines:
         elems = line.split(",")
 
@@ -20,27 +20,25 @@ if __name__ == "__main__":
 
         if elems[0] == "suma":
             for elem in range(len(elems)-2):
-                #print(str(result) + " + " + str(elems[elem+2]))
+                # print(str(result) + " + " + str(elems[elem+2]))
                 result = calcoohija.CalculadoraHija().plus(int(elems[elem+2]), result)
-            
-            
+
         elif elems[0] == "resta":
             for elem in range(len(elems)-2):
-                #print(str(result) + " - " + str(elems[elem+2]))
+                # print(str(result) + " - " + str(elems[elem+2]))
                 result = calcoohija.CalculadoraHija().minus(result, int(elems[elem+2]))
-            
 
         elif elems[0] == "multiplica":
             for elem in range(len(elems)-2):
-                #print(str(result) + " * " + str(elems[elem+2]))
+                # print(str(result) + " * " + str(elems[elem+2]))
                 result = calcoohija.CalculadoraHija().multiply(int(elems[elem+2]), result)
-            
+
         elif elems[0] == "divide":
             for elem in range(len(elems)-2):
-                #print(str(result) + " / " + str(elems[elem+2]))
+                # print(str(result) + " / " + str(elems[elem+2]))
                 result = calcoohija.CalculadoraHija().divide(result, int(elems[elem+2]))
-            
+
         else:
-            sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
+            sys.exit('Solo sumar, restar, multiplicar o dividir.')
 
         print(result)
